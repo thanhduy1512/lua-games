c83cf67ca15c41e96a61c2f8574eb88fa38543db
dexter633 <dexter@nexondv.com>
2023-04-04 14:37:12 +0700

    init src breakout

---
+++ breakout/main.lua
@@ -0,0 +1,22 @@
+require 'src/Dependencies'
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    math.randomseed(os.time())
+
+    love.window.setTitle('Breakout')
+
+    gFonts = {
+        ['small'] = love.graphics.newFont('fonts/font.ttf', 8),
+        ['medium'] = love.graphics.newFont('fonts/font.ttf', 16),
+        ['large'] = love.graphics.newFont('fonts/font.ttf', 32)
+    }
+    love.graphics.setFont(gFonts['small'])
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        vsync = true,
+        fullscreen = false,
+        resizable = true
+    })
+end
